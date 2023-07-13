b2dfe59c1644ca493bd7cd3efa12c5b7fa6bb33b
Mikhail Ustinov <mikhailu359@gmail.com>
2023-07-13 17:42:54 +0300

    hw12 update2

---
+++ src/test/java/com/justforce359/tests/HwPageObjectsRemoteTest.java
@@ -0,0 +1,56 @@
+package com.justforce359.tests;
+
+
+import com.justforce359.pages.RegistrationPage;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+
+public class HwPageObjectsRemoteTest extends TestBaseRemote {
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @Test
+    @Tag("remote")
+    void fillFormTest() {
+        String firstName = testDataGenerator.firstName;
+        String lastName = testDataGenerator.lastName;
+        String userEmail = testDataGenerator.userEmail;
+        String userGender = testDataGenerator.userGender;
+        String phoneNumber = testDataGenerator.phoneNumber;
+        String rDay = testDataGenerator.rDay;
+        String rMonth = testDataGenerator.rMonth;
+        String rYear = testDataGenerator.rYear;
+        String subjects = testDataGenerator.subjects;
+        String hobbies = testDataGenerator.hobbies;
+        String randomPic = testDataGenerator.randomPic;
+        String currentAdd = testDataGenerator.currentAdd;
+        String randomState = testDataGenerator.randomState;
+        String randomCity = testDataGenerator.randomCity;
+
+        registrationPage.openPage()
+                .setFirstname(firstName)
+                .setLastname(lastName)
+                .setUserEmail(userEmail)
+                .setGender(userGender)
+                .userNumber(phoneNumber)
+                .setBirthDate(rDay, rMonth, rYear)
+                .setSubjectsInput(subjects)
+                .hobbiesInput(hobbies)
+                .uploadPic(randomPic)
+                .currentAdd(currentAdd)
+                .stateAndCityInput(randomState, randomCity)
+                .submitClick();
+
+        registrationPage.shouldHave("Thanks for submitting the form")
+                .resultFormCheck("Student Name", firstName + " " + lastName)
+                .resultFormCheck("Student Email", userEmail)
+                .resultFormCheck("Gender", userGender)
+                .resultFormCheck("Mobile", phoneNumber)
+                .resultFormCheck("Date of Birth", rDay + " " + rMonth + "," + rYear)
+                .resultFormCheck("Subjects", subjects)
+                .resultFormCheck("Hobbies", hobbies)
+                .resultFormCheck("Picture", randomPic)
+                .resultFormCheck("Address", currentAdd)
+                .resultFormCheck("State and City", randomState + " " + randomCity);
+    }
+}
